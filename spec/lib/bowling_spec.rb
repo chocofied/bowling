4134440b9b9d27909b848092f0b8d4a55e837d71
EC2 Default User <ec2-user@ip-172-31-15-239.us-east-2.compute.internal>
2021-08-04 01:43:03 +0000

    refactored

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,28 +1,30 @@
 require "bowling"
 
 describe "Bowling score total" do
-    describe "全体の合計" do
-        context "すべての投球がガターだった場合" do
-            it "0になること" do
-                @game = Bowling.new
-                
-                20.times do
-                    @game.add_score(0)
-                end
-        
+    describe "Overall total" do
+        #combine creation of instances
+        before do
+            @game = Bowling.new
+        end
+        context "All tosses were gutter balls" do
+            it "Score is 0" do
+                add_many_scores(20,0)
                 expect(@game.total_score).to eq 0
             end
         end
         
-        context "すべての投球で1ビンずつ倒した場合" do
-            it "20になること" do
-                @game = Bowling.new
-                20.times do
-                    @game.add_score(1)
-                end
-                
+        context "All tosses knocked over 1 pin" do
+            it "Score is 20" do
+                add_many_scores(20,1)
                 expect(@game.total_score).to eq 20
             end
         end
+    end
+end
+private
+#Run multiple times at once with same score
+def add_many_scores(count, pins)
+    count.times do
+        @game.add_score(pins)
     end
 end
